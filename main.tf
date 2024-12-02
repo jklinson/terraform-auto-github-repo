815711a2d29637584cd3a60c8f31551d75ca0514
Linson Alfred <jklinson@gmail.com>
2024-12-02 23:38:26 +1000

    refactor(auto-repo-creation): update tf

--- main.tf
+++ main.tf
@@ -6,6 +6,14 @@ resource "github_repository" "repositories" {
   private     = each.value["private"]
   is_template = each.value["isTemplate"]
 
+  dynamic "template" {
+    for_each = try([each.value["template"]], [])
+    content {
+      owner      = template.value["owner"]
+      repository = template.value["repository"]
+    }
+  }
+
   # Ensure repository is not created if already exists
   lifecycle {
     prevent_destroy = true
